48b76225094519ba120c74c75496f5c0eff8c212
Mauricio Rincón <causemao@gmail.com>
2017-08-09 13:32:31 -0500

    Update contenido_resolucion.go

--- controllers/contenido_resolucion.go
+++ controllers/contenido_resolucion.go
@@ -1,7 +1,7 @@
 package controllers
 
 import (
-  	"github.com/udistrital/administrativa_crud_api/models"
+  	"administrativa_crud_api/models"
   	"encoding/json"
 	"strconv"
 	"fmt"
@@ -19,7 +19,13 @@ func (c *ResolucionCompletaController) URLMapping() {
 	c.Mapping("ResolucionTemplate", c.ResolucionTemplate)
 }
 
-
+// GetOne ...
+// @Title Get Template
+// @Description get ResolucionCompleta by id
+// @Param	idResolucion		path 	string	true		"The key for staticblock"
+// @Success 200 {object} models.ResolucionCompleta
+// @Failure 403
+// @router /ResolucionTemplate [get]
 func (c *ResolucionCompletaController) ResolucionTemplate() {
     resolucion := models.GetTemplateResolucion()
     c.Ctx.Output.SetStatus(201)
@@ -27,6 +33,13 @@ func (c *ResolucionCompletaController) ResolucionTemplate() {
     c.ServeJSON()
 }
 
+// GetOne ...
+// @Title Get One
+// @Description get ResolucionCompleta by id
+// @Param	idResolucion		path 	string	true		"The key for staticblock"
+// @Success 200 {object} models.ResolucionCompleta
+// @Failure 403 :idResolucion is empty
+// @router /:idResolucion [get]
 func (c *ResolucionCompletaController) GetOne() {
 	idResolucion := c.Ctx.Input.Param(":idResolucion")
     resolucion := models.GetOneResolucionCompleta(idResolucion)
@@ -35,6 +48,13 @@ func (c *ResolucionCompletaController) GetOne() {
     c.ServeJSON()
 }
 
+// Put ...
+// @Title Put
+// @Description update the ResolucionCompleta
+// @Param	idResolucion		path 	string	true		"The id you want to update"
+// @Success 200 {object} models.ResolucionCompleta
+// @Failure 403 :idResolucion is not int
+// @router /:idResolucion [put]
 func (c *ResolucionCompletaController) Put() {
 	idResolucionStr := c.Ctx.Input.Param(":idResolucion")
 	idResolucion, _ := strconv.Atoi(idResolucionStr)

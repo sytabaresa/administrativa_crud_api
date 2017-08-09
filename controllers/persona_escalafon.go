e33e975780410672eb9257cc360aa2575bad9025
Mauricio Rincón <causemao@gmail.com>
2017-08-09 13:34:23 -0500

    Update persona_escalafon.go

--- controllers/persona_escalafon.go
+++ controllers/persona_escalafon.go
@@ -1,8 +1,8 @@
 package controllers
 
 import (
-	"github.com/astaxie/beego"
-	"github.com/udistrital/administrativa_crud_api/models"
+  "administrativa_crud_api/models"
+  "github.com/astaxie/beego"
 )
 
 type PersonaEscalafonController struct {
@@ -14,9 +14,15 @@ func (c *PersonaEscalafonController) URLMapping() {
 
 }
 
+// GetAll ...
+// @Title Get All
+// @Description get PersonaEscalafon
+// @Success 200 {object} models.PersonaEscalafon
+// @Failure 403
+// @router / [get]
 func (c *PersonaEscalafonController) GetAll() {
-	listaPersonas := models.GetAllPersonaEscalafon()
-	c.Ctx.Output.SetStatus(201)
-	c.Data["json"] = listaPersonas
-	c.ServeJSON()
+    listaPersonas := models.GetAllPersonaEscalafon()
+    c.Ctx.Output.SetStatus(201)
+    c.Data["json"] = listaPersonas
+    c.ServeJSON()
 }
